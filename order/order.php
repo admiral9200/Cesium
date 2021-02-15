9c62b4383e271e98c049e51b8e92f46418f6ca09
Nicholas Wong <sword9200@gmail.com>
2021-02-15 22:03:03 +0200

    Refactoring orders code

--- order/order.php
+++ order/order.php
@@ -1,39 +1,47 @@
 <?php
 session_start();
-if (!isset($_SESSION['email'])) header('location: /');
+
+if (!isset($_SESSION['email'])) {
+    http_response_code(400);
+    header("location: /");
+}
+
 include("../config/db.connect.php");
 
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'POST':
-        echo orderAgain();
-        break;
+        
+        header("Content-Type: application/json");
+        $req = json_decode(file_get_contents('php://input'));
+
+        echo json_encode(orderAgain());
+        exit();
+
     case 'GET':
-        if (isset($_GET['coffees'])){
-			$fetchCoffees = $pdo -> query('SELECT * FROM cc_coffees');
-			echo json_encode($fetchCoffees -> fetchAll());
+        if (isset($_GET['coffees'])) {
+			echo json_encode(fetchCoffees());
 		}
-        break;
-    default:
-        echo json_encode($res['status'] = 'fail');
-        break;
+        exit();
+}
+
+function fetchCoffees() {
+    global $pdo;
+
+    $queryResolved = $stmtCoffees = $pdo -> query('SELECT * FROM cc_coffees');
+
+    return $queryResolved ? $stmtCoffees -> fetchAll() : ['error' => 'Database Error'];
 }
 
 function orderAgain(){
     global $pdo;
 
-    header("Content-Type: application/json");
-
-    $order = file_get_contents('php://input');
-    $order = json_decode($order);
-
     //Get products of order selected
     $sqlOrderAgain = "SELECT * FROM cc_orders_products WHERE id= ?";
-
     $stmtOrderAgain = $pdo -> prepare($sqlOrderAgain);
-    $queryResolved =  $stmtOrderAgain -> execute([$order -> code]);
+    $queryResolved =  $stmtOrderAgain -> execute([$req -> code]);
 
-    if ($queryResolved) {
-        return json_encode($stmtOrderAgain -> fetchAll());
-    }
-    return json_encode($res['status'] = 'fail');
+    return $queryResolved ? $stmtOrderAgain -> fetchAll() : ['error' => 'Database Error'];
 }
+
+http_response_code(400);
+exit();
