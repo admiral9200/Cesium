0c42bfc80485ea1927296d55c632c11510ec53c9
Nicholas Wong <sword9200@gmail.com>
2021-04-25 22:54:54 +0300

    Cleanup

--- client/src/components/Register/Form.vue
+++ client/src/components/Register/Form.vue
@@ -1,53 +1,51 @@
 <template>
-	<div>
-		<div class="signup-space">
-			<form v-on:submit.prevent="SignUp" novalidate>
-				<div class="row">
-					<div class="col">
-						<div class="group py-2">
-							<label class="label">Email</label>
-							<input v-model.trim="$v.email.$model" :class="{ 'error' : $v.email.$error }" type="email" class="input form-control border-0" placeholder="Γράψε τη Διέυθυνση Email" required>
-							<div v-if="!$v.email.required && $v.email.$dirty" class="text-danger">Πρέπει να συμπληρώσεις το email σου.</div>
-							<div v-if="!$v.email.email && $v.email.$dirty" class="text-danger">Πρέπει να συμπληρώσεις μία έγκυρη διεύθυνση email.</div>
-						</div>
-						<div class="group py-2">
-							<label class="label">Όνομα</label> 
-							<input v-model.trim="$v.name.$model" :class="{ 'error' : $v.name.$error }" type="text" class="input form-control border-0" data-type="firstName" placeholder="Γράψε το Όνομα σου" required>
-							<div v-if="!$v.name.required && $v.name.$dirty" class="text-danger">Πρέπει να συμπληρώσεις το όνομα σου</div>
-							<div v-if="!$v.name.alpha && $v.name.$dirty" class="text-danger">Το όνομα πρέπει να περιέχει μόνο γράμματα</div>
-						</div>
-						<div class="group py-2">
-							<label class="label">Επίθετο</label> 
-							<input v-model.trim="$v.surname.$model" :class="{ 'error' : $v.surname.$error }" type="text" class="input form-control border-0" data-type="lastName" placeholder="Γράψε το Επίθετο σου" required>
-							<div v-if="!$v.surname.required && $v.surname.$dirty" class="text-danger">Πρέπει να συμπληρώσεις το επίθετο σου.</div>
-							<div v-if="!$v.surname.alpha && $v.surname.$dirty" class="text-danger">Το επίθετο πρέπει να περιέχει μόνο γράμματα</div>
-						</div>
-						<div class="group py-2">
-							<label class="label">Κωδικός</label> 
-							<input v-model.trim="$v.password.$model" :class="{ 'error' : $v.password.$error }" type="password" class="input form-control border-0" data-type="password" placeholder="Γράψε τον κωδικό σου" required>
-							<div v-if="!$v.password.required && $v.password.$dirty" class="text-danger">Πρέπει να συμπληρώσεις ένα κωδικό πρόσβασης</div>
-							<div v-if="!$v.password.minLength && $v.password.$dirty" class="text-danger">Ο κωδικός πρόσβασης πρέπει να έιναι μεγαλύτερος από 9 χαρακτήρες</div>
-						</div>
-						<div class="group py-2">
-							<label class="label">Επαλήθευση Κωδικού</label> 
-							<input v-model.trim="$v.passwordConfirm.$model" :class="{ 'error' : $v.passwordConfirm.$error }" type="password" class="input form-control border-0" data-type="password" placeholder="Γράψε ξανά τον κωδικό σου" required>
-							<div v-if="!$v.passwordConfirm.required && $v.passwordConfirm.$dirty" class="text-danger">Πρέπει να συμπληρώσεις ξανά τον κωδικό πρόσβασης.</div>
-							<div v-if="$v.passwordConfirm.required && !$v.passwordConfirm.sameAsPassword && $v.passwordConfirm.$dirty" class="text-danger">Ο κωδικός δεν είναι ίδιος με τον προηγούμενο.</div>
-						</div>
-						<div class="group py-2">
-							<div class="form-check pointer-base">
-								<input @change="$v.terms.$touch()" v-model="terms" type="checkbox" class="form-check-input" id="termsAccept" required>
-								<label class="custom-control-label pointer-base ms-1" for="termsAccept">Αποδέχομαι τους όρους χρήσης</label>
-								<div v-if="!$v.terms.sameAs && $v.terms.$dirty" class="text-danger">Πρέπει να αποδεχτείς τους όρους χρήσης.</div>
-							</div>
-						</div>
-						<div class="d-grid mt-4 py-2">
-							<button type="submit" class="btn btn-block text-white mainbtn">Εγγραφή</button>
-						</div> 
+	<div class="signup-space">
+		<form v-on:submit.prevent="SignUp" novalidate>
+			<div class="row">
+				<div class="col">
+					<div class="group py-2">
+						<label class="label">Email</label>
+						<input v-model.trim="$v.email.$model" :class="{ 'error' : $v.email.$error }" type="email" class="input form-control border-0" placeholder="Γράψε τη Διέυθυνση Email" required>
+						<div v-if="!$v.email.required && $v.email.$dirty" class="text-danger">Πρέπει να συμπληρώσεις το email σου.</div>
+						<div v-if="!$v.email.email && $v.email.$dirty" class="text-danger">Πρέπει να συμπληρώσεις μία έγκυρη διεύθυνση email.</div>
 					</div>
+					<div class="group py-2">
+						<label class="label">Όνομα</label> 
+						<input v-model.trim="$v.name.$model" :class="{ 'error' : $v.name.$error }" type="text" class="input form-control border-0" data-type="firstName" placeholder="Γράψε το Όνομα σου" required>
+						<div v-if="!$v.name.required && $v.name.$dirty" class="text-danger">Πρέπει να συμπληρώσεις το όνομα σου</div>
+						<div v-if="!$v.name.alpha && $v.name.$dirty" class="text-danger">Το όνομα πρέπει να περιέχει μόνο γράμματα</div>
+					</div>
+					<div class="group py-2">
+						<label class="label">Επίθετο</label> 
+						<input v-model.trim="$v.surname.$model" :class="{ 'error' : $v.surname.$error }" type="text" class="input form-control border-0" data-type="lastName" placeholder="Γράψε το Επίθετο σου" required>
+						<div v-if="!$v.surname.required && $v.surname.$dirty" class="text-danger">Πρέπει να συμπληρώσεις το επίθετο σου.</div>
+						<div v-if="!$v.surname.alpha && $v.surname.$dirty" class="text-danger">Το επίθετο πρέπει να περιέχει μόνο γράμματα</div>
+					</div>
+					<div class="group py-2">
+						<label class="label">Κωδικός</label> 
+						<input v-model.trim="$v.password.$model" :class="{ 'error' : $v.password.$error }" type="password" class="input form-control border-0" data-type="password" placeholder="Γράψε τον κωδικό σου" required>
+						<div v-if="!$v.password.required && $v.password.$dirty" class="text-danger">Πρέπει να συμπληρώσεις ένα κωδικό πρόσβασης</div>
+						<div v-if="!$v.password.minLength && $v.password.$dirty" class="text-danger">Ο κωδικός πρόσβασης πρέπει να έιναι μεγαλύτερος από 9 χαρακτήρες</div>
+					</div>
+					<div class="group py-2">
+						<label class="label">Επαλήθευση Κωδικού</label> 
+						<input v-model.trim="$v.passwordConfirm.$model" :class="{ 'error' : $v.passwordConfirm.$error }" type="password" class="input form-control border-0" data-type="password" placeholder="Γράψε ξανά τον κωδικό σου" required>
+						<div v-if="!$v.passwordConfirm.required && $v.passwordConfirm.$dirty" class="text-danger">Πρέπει να συμπληρώσεις ξανά τον κωδικό πρόσβασης.</div>
+						<div v-if="$v.passwordConfirm.required && !$v.passwordConfirm.sameAsPassword && $v.passwordConfirm.$dirty" class="text-danger">Ο κωδικός δεν είναι ίδιος με τον προηγούμενο.</div>
+					</div>
+					<div class="group py-2">
+						<div class="form-check pointer-base">
+							<input @change="$v.terms.$touch()" v-model="terms" type="checkbox" class="form-check-input" id="termsAccept" required>
+							<label class="custom-control-label pointer-base ms-1" for="termsAccept">Αποδέχομαι τους όρους χρήσης</label>
+							<div v-if="!$v.terms.sameAs && $v.terms.$dirty" class="text-danger">Πρέπει να αποδεχτείς τους όρους χρήσης.</div>
+						</div>
+					</div>
+					<div class="d-grid mt-4 py-2">
+						<button type="submit" class="btn btn-block text-white mainbtn">Εγγραφή</button>
+					</div> 
 				</div>
-			</form>
-		</div>
+			</div>
+		</form>
 	</div>
 </template>
 
@@ -94,8 +92,6 @@ export default {
 			name: '',
 			surname: '',
 			terms: false,
-			termsButtonNotChecked: false,
-			hasErrorMsg: '',
 		}
 	},
 
@@ -113,12 +109,10 @@ export default {
 		},
 
 		SignUp: async function() {
-			NProgress.start();
 			this.$v.$touch();
 
 			if(!this.$v.$invalid){
-				this.hasErrorMsg = '';
-
+				NProgress.start();
 				try {
 					const response = await fetch('http://localhost:3000/auth/register', {
 						method: 'POST',
