633e4927afb606ee6003bc195787d1555da5fee2
Nicholas Wong <sword9200@gmail.com>
2021-06-26 21:13:02 +0300

    Delete jwt.config.js

--- server/utils/jwt.config.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const public_key = fs.readFileSync('C:\\Users\\z3r0Luck\\Documents\\Personal_Projects\\Project_Cesium\\server\\utils\\public.key', 'utf-8');
-const private_key  = fs.readFileSync('C:\\Users\\z3r0Luck\\Documents\\Personal_Projects\\Project_Cesium\\server\\utils\\private.key', 'utf8');
-
-module.exports = {
-	'secret': private_key,
-	'public': public_key
-};
