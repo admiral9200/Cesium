837ed96cb6b46c246b8beb176db07af19646ef0b
Nicholas Wong <sword9200@gmail.com>
2021-07-01 04:42:30 +0300

    completely removed mysql from project

--- docker/MySQL.Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM mysql:latest
-
-ADD init.db.sh ./init.db.sh
-ADD chip_coffee.sql /tmp/chip_coffee.sql		
-
-EXPOSE 3306
--- docker/init.db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mysql -u root -e "CREATE DATABASE chip_coffee"
-mysql -u root chip_coffee < /tmp/chip_coffee.sql
